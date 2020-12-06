67d1b7cf6dd2cc4928e925808cf2417b6df6ed92
Muratcan Akçay <muratcanakcay@gmail.com>
2020-12-06 13:05:58 +0100

    Update Program.cs

--- Day06/Program.cs
+++ Day06/Program.cs
@@ -8,12 +8,11 @@ namespace Day06
     {
         static void Main(string[] args)
         {
-            List < List < List<char> >> data = new List<List<List<char>>>();
-
             string[] lines = System.IO.File.ReadAllLines(@"C:\CodeBase\VS19\AdventOfCode2020\Day06\data.txt"); // ***** change this to the location of data.txt *****
-            
+
             // get data from file
 
+            List<List<List<char>>> data = new List<List<List<char>>>(); 
             List<List<char>> groupdata = new List<List<char>>();
 
             foreach (var line in lines)
@@ -28,10 +27,8 @@ namespace Day06
                 List<char> persondata = new List<char>();
 
                 for (int i = 0; i < line.Length; i++)
-                {
                     persondata.Add(line[i]);
-                }
-
+                
                 groupdata.Add(persondata);
             }
 
@@ -41,39 +38,36 @@ namespace Day06
             
             int sumanyone = 0;
             int sumeveryone = 0;
+            int subtotal = 0;
 
             foreach (var group in data)
             {
+                subtotal = 0;
                 List<char> grouplist = new List<char>();
 
-                // part 1
-                // prepare group list using yes answers from everyone
+                // part 1 - prepare group list using yes answers from anyone
 
                 foreach (var person in group)
-                {
                     foreach (var ch in person)
                         if (!grouplist.Contains(ch)) grouplist.Add(ch);
-                }
+                
+                subtotal = grouplist.Count(); // yes answers by anyone
 
-                sumanyone += grouplist.Count(); // add number of yes answers to total
+                sumanyone += subtotal; 
 
-                // part 2
-                // remove answer from group list if someone has not answered yes
+                // part 2 - remove answer if someone has not answered yes
 
                 List<char> copylist = new List<char>(grouplist);
 
                 foreach (var ch in grouplist)
-                {
                     foreach (var person in group)
-                    {
                         if (!person.Contains(ch))
                         {
-                            copylist.Remove(ch);
+                            subtotal--; 
+                            break; // move on to check if next answer was yes for everyone
                         }
-                    }
-                }
-
-                sumeveryone += copylist.Count(); // add number of yes answers to total
+                
+                sumeveryone += subtotal;
             }
 
             Console.WriteLine($"Total answers anyone answered yes to: {sumanyone}");
